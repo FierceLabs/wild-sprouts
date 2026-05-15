dff985dcba25203f45eb7d4b755b7fc19188b0b3
Jared Nelson <roial33@hotmail.com>
2026-05-15 09:49:41 -0600

    fixed

--- next.config.js
+++ next.config.js
@@ -4,6 +4,36 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
   },
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/hls/:path*.m3u8',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/vnd.apple.mpegurl',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/hls/:path*.ts',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'video/mp2t',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
